eb6688757a4c9bdaf364cabc1e9c5e14f20dd4a2
bartytime4life <blackbart555@gmail.com>
2026-04-27 09:33:59 -0500

    Delete apps/governed-api directory

--- apps/governed-api/ecology/evidencebundle_resolver.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from apps.governed_api.ecology.evidencebundle_resolver import *  # noqa: F401,F403
--- apps/governed-api/ecology/fastapi_routes.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from apps.governed_api.ecology.fastapi_routes import *  # noqa: F401,F403
