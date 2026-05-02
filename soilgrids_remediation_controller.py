601ba9aa1f8f0a6578d81ae5532c5a5e1daef555
bartytime4life <blackbart555@gmail.com>
2026-05-02 13:17:07 -0500

    Delete soilgrids_remediation_controller.py

--- soilgrids_remediation_controller.py
+++
@@ -1,5 +0,0 @@
-from tools.soilgrids.soilgrids_remediation_controller import *  # noqa: F401,F403
-
-if __name__ == "__main__":
-    from runpy import run_module
-    run_module("tools.soilgrids.soilgrids_remediation_controller", run_name="__main__")
