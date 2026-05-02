1229df15d87066419a549cc3a104d8de847aa178
bartytime4life <blackbart555@gmail.com>
2026-05-02 13:24:38 -0500

    Delete soilgrids_assurance_pack.py

--- soilgrids_assurance_pack.py
+++
@@ -1,5 +0,0 @@
-from tools.soilgrids.soilgrids_assurance_pack import *  # noqa: F401,F403
-
-if __name__ == "__main__":
-    from runpy import run_module
-    run_module("tools.soilgrids.soilgrids_assurance_pack", run_name="__main__")
