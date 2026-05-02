487367533e8c4fc85015711e33de6893e3f4af7f
bartytime4life <blackbart555@gmail.com>
2026-05-02 13:16:37 -0500

    Delete soilgrids_supply_chain.py

--- soilgrids_supply_chain.py
+++
@@ -1,5 +0,0 @@
-from tools.soilgrids.soilgrids_supply_chain import *  # noqa: F401,F403
-
-if __name__ == "__main__":
-    from runpy import run_module
-    run_module("tools.soilgrids.soilgrids_supply_chain", run_name="__main__")
