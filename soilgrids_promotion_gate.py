8c0735f9a3c478bb0477fddd134374f11d5fd50d
bartytime4life <blackbart555@gmail.com>
2026-05-02 13:22:29 -0500

    Delete soilgrids_promotion_gate.py

--- soilgrids_promotion_gate.py
+++
@@ -1,5 +0,0 @@
-from tools.soilgrids.soilgrids_promotion_gate import *  # noqa: F401,F403
-
-if __name__ == "__main__":
-    from runpy import run_module
-    run_module("tools.soilgrids.soilgrids_promotion_gate", run_name="__main__")
