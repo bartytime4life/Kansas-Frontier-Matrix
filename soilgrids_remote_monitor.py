5ab98bf4ec02f35e09871ffeebca76818e423c4f
bartytime4life <blackbart555@gmail.com>
2026-05-02 13:16:59 -0500

    Delete soilgrids_remote_monitor.py

--- soilgrids_remote_monitor.py
+++
@@ -1,5 +0,0 @@
-from tools.soilgrids.soilgrids_remote_monitor import *  # noqa: F401,F403
-
-if __name__ == "__main__":
-    from runpy import run_module
-    run_module("tools.soilgrids.soilgrids_remote_monitor", run_name="__main__")
