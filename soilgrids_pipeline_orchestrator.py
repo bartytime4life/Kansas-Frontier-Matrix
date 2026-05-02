fc44c2c2317c2caed0d867b1869e83acb7b48687
bartytime4life <blackbart555@gmail.com>
2026-05-02 13:22:42 -0500

    Delete soilgrids_pipeline_orchestrator.py

--- soilgrids_pipeline_orchestrator.py
+++
@@ -1,5 +0,0 @@
-from tools.soilgrids.soilgrids_pipeline_orchestrator import *  # noqa: F401,F403
-
-if __name__ == "__main__":
-    from runpy import run_module
-    run_module("tools.soilgrids.soilgrids_pipeline_orchestrator", run_name="__main__")
