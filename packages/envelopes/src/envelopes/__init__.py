6cf2b18eda527e26c946b604b91cec3f713e1816
Andy <blackbart555@gmail.com>
2026-08-14 18:02:37 -0500

    feat(envelopes): add deterministic AIReceipt candidate builder

--- packages/envelopes/src/envelopes/__init__.py
+++ packages/envelopes/src/envelopes/__init__.py
@@ -1,5 +1,6 @@
 """Supported exports for the bounded KFM envelope-candidate helper package."""
 
+from .ai_receipt import AI_RECEIPT_OUTCOMES, build_ai_receipt_candidate
 from .map_context_evidence_drawer import (
     ADAPTER_VERSION,
     DRAWER_PROFILE,
@@ -15,11 +16,13 @@ from .runtime_response import (
 
 __all__ = [
     "ADAPTER_VERSION",
+    "AI_RECEIPT_OUTCOMES",
     "DRAWER_PROFILE",
     "EVIDENCE_KINDS",
     "MAP_CONTEXT_PROFILE",
     "OUTCOMES",
     "EnvelopeBuildError",
+    "build_ai_receipt_candidate",
     "build_map_context_evidence_drawer_admission_candidate",
     "build_runtime_response_candidate",
 ]
