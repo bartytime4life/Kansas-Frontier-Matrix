11db0bdfe51bf7ae6d3a14c0fa91c99bf5126c7a
Andy <blackbart555@gmail.com>
2026-09-01 08:19:48 -0500

    feat(maplibre): export temporal playback coordinator

--- packages/maplibre/src/index.ts
+++ packages/maplibre/src/index.ts
@@ -3,4 +3,5 @@ export * from "./map-runtime-navigation-state";
 export * from "./map-runtime-navigation-history";
 export * from "./map-runtime-resize-coordinator";
 export * from "./map-runtime-camera-coordinator";
+export * from "./map-runtime-temporal-playback";
 export * from "./null-map-runtime";
