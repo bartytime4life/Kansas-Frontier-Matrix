86694a3d731f84848c3e7b0581dd5fff13234a50
Andy <blackbart555@gmail.com>
2026-09-01 07:18:09 -0500

    feat(maplibre): export camera coordinator

--- packages/maplibre/src/index.ts
+++ packages/maplibre/src/index.ts
@@ -2,4 +2,5 @@ export * from "./map-runtime-port";
 export * from "./map-runtime-navigation-state";
 export * from "./map-runtime-navigation-history";
 export * from "./map-runtime-resize-coordinator";
+export * from "./map-runtime-camera-coordinator";
 export * from "./null-map-runtime";
